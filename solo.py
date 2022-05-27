6da90a1bd2f1425db366822ed47d64ba356c97e4
TuanHung <th1@th.localdomain>
2022-05-27 16:17:34 +0700

    add lên :))

---
+++ solo.py
@@ -0,0 +1,186 @@
+from random import randint
+
+
+print("Số lô ngày hôm nay")
+
+print("[42 / 56 / 79 / 69 / 21 / 43 / 49]")
+
+print()
+
+print("Mua con lô nào.")
+
+print("Lựa chọn của bạn là:")
+
+
+
+
+
+you= input()
+computer= randint(1,2,)
+
+
+
+#máy tính chọn
+
+if computer==1:
+    computer="42"
+if computer==2:
+    computer="56"
+if computer==3:
+    computer="79"
+if computer==4:
+    computer="69"
+if computer==5:
+    computer="21"
+if computer==6:
+    computer="43"
+if computer==7:
+    computer="49"
+
+
+
+
+#chơi xổ số
+print("Đang cập nhật kết quả")
+print("----------cập nhật thành công----------")
+
+
+print("Kết quả xổ số:" + computer)
+
+
+print("Bạn đã chọn : " + you)
+#lần 1
+if you=="42" :
+    if computer=="42":
+        print("Bạn đã trúng, xin chúc mừng, truy cập https://www.youtube.com/watch?v=dQw4w9WgXcQ để nhận 1.000.000.000VND")
+    if computer=="56":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="79":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="69":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="21":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="43":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="49":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+
+        
+
+#lần 2
+elif you=="56" :
+    if computer=="56":
+        print("Bạn đã trúng, xin chúc mừng, truy cập https://www.youtube.com/watch?v=dQw4w9WgXcQ để nhận 1.000.000.000VND")
+    if computer=="42":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="79":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="69":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="21":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="43":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="49":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+
+#lần 3
+elif you=="79" :
+    if computer=="79":
+        print("Bạn đã trúng, xin chúc mừng, truy cập https://www.youtube.com/watch?v=dQw4w9WgXcQ để nhận 1.000.000.000VND")
+    if computer=="56":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="42":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="69":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="21":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="43":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="49":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+
+
+
+#lần 4
+elif you=="69" :
+    if computer=="69":
+        print("Bạn đã trúng, xin chúc mừng, truy cập https://www.youtube.com/watch?v=dQw4w9WgXcQ để nhận 1.000.000.000VND")
+    if computer=="56":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="79":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="42":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="21":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="43":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="49":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+
+    
+
+#lần 5
+elif you=="21" :
+    if computer=="21":
+        print("Bạn đã trúng, xin chúc mừng, truy cập https://www.youtube.com/watch?v=dQw4w9WgXcQ để nhận 1.000.000.000VND")
+    if computer=="56":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="79":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="69":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="42":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="43":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="49":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+
+
+
+#lần 6
+elif you=="43" :
+    if computer=="43":
+        print("Bạn đã trúng, xin chúc mừng, truy cập https://www.youtube.com/watch?v=dQw4w9WgXcQ để nhận 1.000.000.000VND")
+    if computer=="56":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="79":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="69":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="21":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="42":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="49":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+
+
+
+
+#lần 7
+elif you=="49" :
+    if computer=="49":
+        print("Bạn đã trúng, xin chúc mừng, truy cập https://www.youtube.com/watch?v=dQw4w9WgXcQ để nhận 1.000.000.000VND")
+    if computer=="56":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="79":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="69":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="21":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="43":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+    if computer=="42":
+        print("Bạn đã không trúng, chúc may mắn lần sau")
+
+# else
+else :
+
+    print("Không có số  " + you +" .")
+    print("Điền lại đê ")
